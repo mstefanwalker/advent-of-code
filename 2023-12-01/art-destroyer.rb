7bcc5f1816802acb2f38d81f48000d7386a9efc0
M Stefan Walker <m.stefan.walker@gmail.com>
2023-12-01 20:07:54 -0500

    2023-12-01 second star

--- 2023-12-01/art-destroyer.rb
+++ 2023-12-01/art-destroyer.rb
@@ -9,8 +9,44 @@ class String
     end
 end
 
+digits = '0123456789'
+
+spelled = {
+    'o' => ['one'],
+    't' => ['two', 'three'],
+    'f' => ['four', 'five'],
+    's' => ['six', 'seven'],
+    'e' => ['eight'],
+    'n' => ['nine'],
+}
+
+translated = {
+    'one' => '1',
+    'two' => '2',
+    'three' => '3',
+    'four' => '4',
+    'five' => '5',
+    'six' => '6',
+    'seven' => '7',
+    'eight' => '8',
+    'nine' => '9',
+}
+
 File.foreach("calibration.txt") do |line|
-    numbers = line.delete "^[0-9]" 
+    numbers = ''
+    for i in 0...line.length
+        char = line[i].chr
+        if digits.include? char
+            numbers += char
+        elsif spelled.has_key? char
+            options = spelled[char]
+            options.each do |option|
+                if line[i, option.length] == option
+                    numbers += translated[option]
+                end
+            end
+        end
+    end
     sum += "#{numbers.first}#{numbers.last}".to_i
 end
 
